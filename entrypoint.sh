57e024b940497b5e97c8a2fe11f4fc519aa2a2ab
GP <ganesh.prasannah@avalara.com>
2019-08-09 11:14:33 +0530

    Wire up exit code for cc-test-reporter after-build.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,10 @@
 
 set -eu
 
-env
-pwd
-ls -lshrta /github/workspace
-
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
 chmod +x ./cc-test-reporter
 ./cc-test-reporter before-build
 
 bash -c "$*"
 
-./cc-test-reporter after-build --exit-code
+./cc-test-reporter after-build --exit-code $?
