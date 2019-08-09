4696974673fa26e71948172cedc14b983ee490e1
GP <ganesh.prasannah@avalara.com>
2019-08-09 10:53:38 +0530

    debug: See if the entrypoint script has WORKDIR set.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -eu
 
 env
 pwd
-ls -lshrta /github
+ls -lshrta /github/workspace
 
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
 chmod +x ./cc-test-reporter
