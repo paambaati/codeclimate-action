06ce90968e228db07230ca175b7934a64cfcf9c4
GP <exchequer598@gmail.com>
2019-10-27 17:18:40 +0530

    scripts: Minor fixes in release script.
    1. Skip Husky hooks.
    2. Force-remove Git hooks.

--- release.sh
+++ release.sh
@@ -6,17 +6,20 @@ set -e
 git_branch=$(git rev-parse --abbrev-ref HEAD)
 if [ "$git_branch" == "master" ]; then
     echo "Cannot release from 'master' branch. Please checkout to a release branch!"
-    echo "Example: git checkout -b v1-release"
+    echo "Example: git checkout -b v2-release"
     exit 1
 fi
 
 # Install dependencies and build & test.
-DISABLE_OPENCOLLECTIVE=true npm install
+DISABLE_OPENCOLLECTIVE=true HUSKY_SKIP_INSTALL=true npm install
 npm test
 npm run build
 
 # Build & tests successful. Now keep only production deps.
 npm prune --production
+
+# Remove Git hooks.
+rm -rf .git/hooks/*
 
 # Force add built files and deps.
 git add --force lib/ node_modules/
@@ -24,7 +27,7 @@ git commit -a -m "Publishing $git_branch"
 git push -u origin $git_branch
 
 # Set up release tag.
-read -p "Enter tag (example: v1.0.0) " git_tag
+read -p "Enter tag (example: v2.2.4) " git_tag
 git push origin ":refs/tags/$git_tag"
 git tag -fa "$git_tag" -m "Release $git_tag"
 git push -u origin $git_tag
