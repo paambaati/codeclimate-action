66520325a87c710f5b7859c7857c493351ba3df4
GP <exchequer598@gmail.com>
2019-10-27 17:22:47 +0530

    Publishing v2-release

--- release.sh
+++ release.sh
@@ -18,20 +18,16 @@ npm run build
 # Build & tests successful. Now keep only production deps.
 npm prune --production
 
-echo 1
 # Remove Git hooks.
 rm -rf .git/hooks/*
-echo 2
+
 # Force add built files and deps.
 git add --force lib/ node_modules/
-echo 3
 git commit -a -m "Publishing $git_branch"
-echo 4
 git push -u origin $git_branch
-echo 5
 
 # Set up release tag.
-read -p "Enter tag (example: v2.2.4) " git_tag
+read -p "Enter tag (example: v2.2.5) " git_tag
 git push origin ":refs/tags/$git_tag"
 git tag -fa "$git_tag" -m "Release $git_tag"
 git push -u origin $git_tag
