c796f651db785c619396cdbb8e09fe0c23f5dc46
GP <exchequer598@gmail.com>
2023-05-06 12:46:03 +0530

    fix(tests): make sure the special variable in script are quoted to make string comparison work

--- test/fixtures/dummy-cc-reporter-after-build-error.bat
+++ test/fixtures/dummy-cc-reporter-after-build-error.bat
@@ -1,6 +1,6 @@
 :: Dummy shell script that exits with a non-zero code when the argument 'after-build' is given.
 @echo off
-IF %* == "after-build --exit-code 0" (
+IF "%*" == "after-build --exit-code 0" (
   EXIT /b 69
 ) ELSE (
   :: `CALL` is a no-op.
